deb8339ee22a9dce42c99be79e2bf8a928f6dc5a
wasiushisir <wasiuahmed410@gmail.com>
2022-10-07 03:38:48 +0600

    first commit

---
+++ src/UserData/UserData.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+
+const UserData = () => {
+    const[data,setData]=useState([])
+    const[filterData,setFilterData]=useState([])
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=>res.json())
+        .then(data=>setData(data))
+
+    },[])
+
+    useEffect(()=>{
+        if(data.length!==0){
+            setFilterData(data)
+        }
+    },[data])
+
+    const searchBox=(event)=>{
+        const serachData=event.target.value;
+        const find=data.filter(d=>d.name && d.email.toLowerCase().includes(serachData.toLowerCase()))
+        setFilterData(find)
+
+    }
+
+
+    return (
+        <div className='container'>
+            <h2 className='text-center mt-3'>User Information</h2>
+            <div className='d-flex justify-content-center  align-items-center mt-4'>
+                
+                <div style={{backgroundColor:'#989898'}} className='d-flex align-items-center ps-2 py-1 '>
+                <MagnifyingGlassIcon  style={{heigth:'25px',width:'25px',color:'white'}} className=""/>
+                <input onChange={searchBox} style={{backgroundColor:'#989898',outline:'none',color:'white'}} className='border border-0 outline-none' type="text" />
+
+                </div>
+               
+            </div>
+            
+
+            <table className='table table-dark table-striped table-border mt-5'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Company Name</th>
+                        <th>Zipcode</th>
+
+
+                    </tr>
+
+                </thead>
+
+                <tbody>
+                   {
+                    filterData.map(d=><tr>
+                        <td>{d.id}</td>
+                        <td>{d.name}</td>
+                        <td>{d.email}</td>
+                        <td>{d.company.name}</td>
+                        <td>{d.address.zipcode}</td>
+                    </tr>)
+                   }
+
+                </tbody>
+
+            </table>
+           
+            
+        </div>
+    );
+};
+
+export default UserData;
